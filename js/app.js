737397163a4e6b099c1fb19c4472ca92e6bf335c
timosha5 <107199949+timosha5@users.noreply.github.com>
2022-09-07 18:21:06 +0300

    part 12

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $(function() {
 
+    /*filter */
 let filter = $("[data-filter]");
 
 filter.on("click", function (event) {
@@ -26,6 +27,65 @@ filter.on("click", function (event) {
     }
 
 });
+
+
+
+/* modal
+ ==========*/
+
+ const modalCall = $("[data-modal]");
+    const modalClose = $("[data-close]");
+
+    modalCall.on("click", function(event) {
+        event.preventDefault();
+
+        let $this = $(this);
+        let modalId = $this.data('modal');
+
+        $(modalId).addClass('show');
+        $("body").addClass('no-scroll');
+
+        setTimeout(function() {
+            $(modalId).find(".modal__dialog").css({
+                transform: "scale(1)"
+            });
+        }, 200);
+    });
+
+
+    modalClose.on("click", function(event) {
+        event.preventDefault();
+
+        let $this = $(this);
+        let modalParent = $this.parents('.modal');
+
+        modalParent.find(".modal__dialog").css({
+            transform: "scale(0)"
+        });
+
+        setTimeout(function() {
+            modalParent.removeClass('show');
+            $("body").removeClass('no-scroll');
+        }, 200);
+    });
+
+
+    $(".modal").on("click", function(event) {
+        let $this = $(this);
+
+        $this.find(".modal__dialog").css({
+            transform: "scale(0)"
+        });
+
+        setTimeout(function() {
+            $this.removeClass('show');
+            $("body").removeClass('no-scroll');
+        }, 200);
+    });
+
+    $(".modal__dialog").on("click", function(event) {
+        event.stopPropagation();
+    });
 
 
 });
